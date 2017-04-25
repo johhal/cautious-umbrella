07ce6a363894a21d496dee8f99be52b3d487b6ab
Pontus <ponjoh90@gmail.com>
2017-04-25 21:02:54 +0200

    Added files

---
+++ Spacecraft/Assets/Script/Ship.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+    // The ship
+    public ShipMovement shipMovement;
+    // For visibility only
+    public double x_position;
+    public double y_position;
+    public double current_speed;
+    public double direction;
+
+    // Use this for initialization
+    void Start () {
+        shipMovement = new ShipMovement(0, 0);
+	}
+
+    // Update is called with fixed time
+    private void FixedUpdate()
+    {
+        shipMovement.move();
+        x_position = shipMovement.x_position;
+        y_position = shipMovement.y_position;
+        current_speed = shipMovement.current_speed;
+        direction = shipMovement.direction;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Spacecraft/Assets/Script/ShipMovement.cs
@@ -0,0 +1,26 @@
+﻿
+public class ShipMovement {
+
+    //keep track of ships speed and movement
+    public double x_position;
+    public double y_position;
+    public double current_speed;
+    public double direction;
+
+    public ShipMovement(double x_position, double y_position)
+    {
+        this.x_position = x_position;
+        this.y_position = y_position;
+        current_speed = 1;
+        direction = System.Math.PI / 4;
+     }
+
+    public void move()
+    {
+        //x-axel arccos
+        x_position = x_position + System.Math.Cos(direction) * current_speed;
+
+        //y-axel arcsin
+        y_position = y_position + System.Math.Sin(direction) * current_speed;
+    }
+}
