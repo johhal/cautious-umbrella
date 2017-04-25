968c29e07cabe71a08657f9d9daeda37751980f8
Pontus <ponjoh90@gmail.com>
2017-04-25 21:15:28 +0200

    NMeedd to fix merge conflicts

---
+++ Spacecraft/Assets/Script/SteeringWheel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringWheel : MonoBehaviour {
+
+    public ShipMovement shipMovement;
+
+    public double turn;
+	// Use this for initialization
+	void Start () {
+		if (shipMovement == null)
+        {
+            shipMovement = GetComponent<ShipMovement>();
+        }
+	}
+	
+	// Update is called once per fixed frame
+	void FixedUpdate () {
+		if (shipMovement != null)
+        {
+            shipMovement.direction += (2 * System.Math.PI) / 360 * turn;
+        }
+	}
+}
