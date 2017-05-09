8f8bb6153dc780f0ba82c5048a82d92a7d766fd7
Pontus <ponjoh90@gmail.com>
2017-05-09 20:06:14 +0200

    Added steering wheel

--- Spacecraft/Assets/Script/SteeringWheel.cs
+++ Spacecraft/Assets/Script/SteeringWheel.cs
@@ -4,30 +4,39 @@ using UnityEngine;
 
 public class SteeringWheel : ActivableObject  {
 
-    public ShipMovement shipMovement;
+    public Ship ship;
 
+    public float labour = 5;
     public double turn;
 	// Use this for initialization
 	void Start () {
-		if (shipMovement == null)
+		if (ship == null)
         {
-            shipMovement = GetComponent<Ship>().shipMovement;
+            ship = GetComponent<Ship>();
             // Obligatory Crapline delux ultra edition.
         }
 	}
 	
 	// Update is called once per fixed frame
 	void FixedUpdate () {
-		if (shipMovement != null)
+        if (ship != null)
         {
-            //shipMovement.direction += (2 * System.Math.PI) / 360 * turn;
-
+            transform.localEulerAngles = new Vector3(0, 0, (float) ship.shipMovement.direction);
         }
 	}
 
     public override bool Activate(PlayerStats playerStats)
     {
-
+        //Check if the steering wheel is connected to a boat...
+        if (ship == null)
+        {
+            return false;
+        }
+        if (playerStats.Current_Stamina >= labour)
+        {
+            playerStats.Current_Stamina -= labour;
+            ship.direction += (2 * System.Math.PI) / 360 * turn;
+        }
         return false;
     }
 }
