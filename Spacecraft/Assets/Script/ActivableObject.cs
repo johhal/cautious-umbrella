fb9efb7ad3ee8b4fd03fad52a160358a8bcb27bc
Pontus <ponjoh90@gmail.com>
2017-05-09 19:33:13 +0200

    Added PlayerStats and ActivableObject

---
+++ Spacecraft/Assets/Script/ActivableObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activateable : MonoBehaviour {
+
+    //Overide this to make the object activable for the player
+    public bool Activate(float PlayerStats)
+    {
+        return false;
+    }
+}
---
+++ Spacecraft/Assets/Script/PlayerStats.cs
@@ -0,0 +1,22 @@
+﻿
+public class PlayerStats{
+    public float Max_Stamina;
+    public float Current_Stamina;
+    public float Stamina_Regen;
+
+    public PlayerStats(float Max_Stamina, float Current_Stamina, float Stamina_Regen)
+    {
+        this.Max_Stamina = Max_Stamina;
+        this.Current_Stamina = Current_Stamina;
+        this.Stamina_Regen = Stamina_Regen;
+    }
+    
+	public void Regenerate_Stamina()
+    {
+        Current_Stamina += Stamina_Regen;
+        if (Current_Stamina > Max_Stamina)
+        {
+            Current_Stamina = Max_Stamina;
+        }
+    }
+}
