d39791e377dbb3acf51fad39a8543d2dbf4a4ff7
Pontus <ponjoh90@gmail.com>
2017-05-16 18:58:37 +0200

    Added ActivableObject focus
    ActivableObject now contains focus gained and lost.
    ActivableObjectfinder now handles focus switches.

--- Spacecraft/Assets/Script/ActivableFinder.cs
+++ Spacecraft/Assets/Script/ActivableFinder.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class ActivableFinder : MonoBehaviour {
     public float MaxDistance = 3;
     // Use this for initialization
+    //The new activeable
     public ActivableObject active;
-
+    //The previous one. Used to identify if it has changed.
+    public ActivableObject oldActive;
     //Access to the player movement
     public PlayerMovement playerMovement;
     //Access to the player stats
@@ -14,8 +16,49 @@ public class ActivableFinder : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        active = getCloseActivable();
+        //Find nearest ActivableObject within range.
+        FindObject();
+        //Toggle focus from old (if any) to new (if any)
+        Focus();
+        //Activate the object if the user is pressing button_a
+        Activate();
+    }
 
+    private void FindObject()
+    {
+        active = getCloseActivable();
+    }
+
+    private void Focus()
+    {
+        //If no new was found and no old was used simply exit.
+        if (oldActive == null && active == null)
+        {
+            return;
+        }
+        //If no old was used and a new was found activate the new one and store it as old.
+        if(oldActive == null && active != null)
+        {
+            oldActive = active;
+            active.FocusGained();
+        }else if(oldActive != null && active == null)
+        {
+            //If old was used and no new was found deactivate the old one and set it to null (the new one).
+            oldActive.FocusLost();
+            oldActive = active;
+        }else
+        {
+            if (oldActive.Equals(active))
+            {
+                oldActive.FocusLost();
+                active.FocusGained();
+                oldActive = active;
+            }
+        }
+    }
+
+    private void Activate()
+    {
         if (active != null)
         {
             if (playerMovement.button_a)
